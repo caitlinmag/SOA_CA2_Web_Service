3f0308043393d43e8a7680098a5d0b8b29125deb
caitlinmag <caitlinmaguire14@outlook.com>
2025-12-11 17:52:58 +0000

    Small change for database
    Corrected the connection string as it changed.

---
+++ SOA_CA2_Web_Service.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SOA_CA2_Web_Service.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SOA_CA2_Web_Service.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("762a2ab2-4c5a-4543-851b-7821f4eea443")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
