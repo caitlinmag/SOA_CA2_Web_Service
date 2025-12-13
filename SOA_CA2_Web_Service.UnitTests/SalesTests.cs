fee30b78d57bc0926a946e994f5632a839564583
caitlinmag <caitlinmaguire14@outlook.com>
2025-12-13 13:06:12 +0000

    Additional unit tests created
    I have made unit tests for all of the CRUD functionality for drinkitems, sales, suppliers and users.

--- SOA_CA2_Web_Service.UnitTests/SalesTests.cs
+++ SOA_CA2_Web_Service.UnitTests/SalesTests.cs
@@ -2,6 +2,7 @@
 using APIService.DTOs;
 using APIService.Interfaces;
 using APIService.Models;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -19,17 +20,71 @@ namespace SOA_CA2_Web_Service.UnitTests
         public async Task CreateSaleTest()
         {
             var testService = new Mock<IDrinkSalesService>();
+            var testController = new DrinkSalesController(testService.Object);
 
-            var newSale = new SaleCreateDTO
+            var saleDto = new SaleCreateDTO
             {
                 DrinkItemId = "6561f5b682e0e02c61099324",
                 Quantity = 3
             };
 
+            // using It.isAny from - https://dev.to/imdj/unit-testing-aspnet-core-web-api-with-moq-and-xunit-controllers-services-nci?utm_source=chatgpt.com
+            testService.Setup(s => s.CreateSale(It.IsAny<DrinksSales>()))
+                .Returns(Task.CompletedTask);
 
-            testService.Setup(s => s.CreateSale(newSale))
-                .ReturnsAsync(mockDrinks)
+            var result = await testController.PostSaleItem(saleDto);
+
+            var createdSale = Assert.IsType<CreatedAtActionResult>(result.Result);
+
+            var sale = Assert.IsType<SaleDTO>(createdSale.Value);
+
+            Assert.Equal(3, sale.Quantity);
         }
+
+
+        // Testing UpdateSale method, results = existing sale has been updated with new values
+        [Fact]
+        public async Task UpdateSaleTest()
+        {
+            var testService = new Mock<IDrinkSalesService>();
+            var testController = new DrinkSalesController(testService.Object);
+
+            var newSaleDetails = new SaleUpdateDTO
+            {
+                DrinkItemId = "6561f5b682e0e02c61099324",
+                Quantity = 4
+            };
+
+            var existingSale = new DrinksSales
+            {
+                DrinksSalesId = "sale1", 
+                DrinkItemId = "6561f5b682e0e02c61099324",
+                Quantity = 10 // updating the quantity amount
+            };
+
+            testService.Setup(s => s.GetSaleById("sale1"))
+                .ReturnsAsync(existingSale);
+
+            testService.Setup(s => s.UpdateSale("sale1", It.IsAny<DrinksSales>()))
+                .Returns(Task.CompletedTask);
+
+            var result = await testController.PutSalesItem("sale1", newSaleDetails);
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        // Testing UpdateSale method if the sale id doesn't exist 
+        [Fact]
+        public async Task UpdateSaleNotFoundTest()
+        {
+            var testService = new Mock<IDrinkSalesService>();
+            var testController = new DrinkSalesController(testService.Object);
+
+            testService.Setup(s => s.GetSaleById("saledoesntexist"))
+                .ReturnsAsync((DrinksSales)null);
+
+            var result = await testController.PutSalesItem("saledoesntexist", new SaleUpdateDTO());
+            Assert.IsType<NotFoundResult>(result);
         }
     }
-}
+    }
+
--- SOA_CA2_Web_Service.UnitTests/SupplierTests.cs
+++ SOA_CA2_Web_Service.UnitTests/SupplierTests.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using APIService;
+using APIService.Controllers;
+using APIService.DTOs;
+using APIService.Interfaces;
+using APIService.Models;
+using APIService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using MongoDB.Driver;
+using Moq;
+using System;
+using Xunit;
 
 namespace SOA_CA2_Web_Service.UnitTests
 {
-    internal class SupplierTests
+    public class SupplierTests
     {
+        // Testing GetSupplierByID method , result = returns supplier id and its values 
+        [Fact]
+        public async Task GetSupplierByIDTest()
+        {
+            var testService = new Mock<ISupplierService>();
+            var testController = new SuppliersController(testService.Object);
+
+            var supplier = new Supplier
+            {
+                SupplierId = "supplier1", 
+                SupplierName = "Insomnia"
+            };
+
+            testService.Setup(s => s.GetSupplierById("supplier1"))
+               .ReturnsAsync(supplier);
+
+            var result = await testController.GetSupplier("supplier1");
+
+            var testSupplier = Assert.IsType<SupplierDTO>(result.Value);
+
+            Assert.Equal("Insomnia", testSupplier.SupplierName);
+        }
+
+        // Testing DeleteSupplier method , supplier should be successfully deleted by id 
+        [Fact]
+        public async Task DeleteSupplierTest()
+        {
+            var testService = new Mock<ISupplierService>();
+            var testController = new SuppliersController(testService.Object);
+
+            var supplier = new Supplier
+            {
+                SupplierId = "supplier2",
+            };
+
+            testService.Setup(s => s.DeleteSupplier("supplier2")).Returns(Task.CompletedTask);
+       
+            var result = await testController.DeleteSupplier("supplier2");
+
+            Assert.IsType<NotFoundResult>(result);
+        }
     }
 }
