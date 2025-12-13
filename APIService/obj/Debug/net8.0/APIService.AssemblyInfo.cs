300900f95543560303108dd02db0243ce0c2441d
caitlinmag <caitlinmaguire14@outlook.com>
2025-12-13 16:41:29 +0000

    Showing drinks data in client
    Working on the client model, drinks data can now be viewed from API.

--- APIService/obj/Debug/net8.0/APIService.AssemblyInfo.cs
+++ APIService/obj/Debug/net8.0/APIService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63b9f8238787729ebe39cacbe13cc1252dc99c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e022aea869fa5e2b4f52a9c2969cc30d162cd1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
